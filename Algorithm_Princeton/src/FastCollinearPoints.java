ab23625d25446ddee1307690defb7544e9c62fb4
nguyentrantrung986 <nguyentrantrung986@gmail.com>
2018-01-02 23:11:53 +0700

    move isCollinear check to outer loop

--- Algorithm_Princeton/src/FastCollinearPoints.java
+++ Algorithm_Princeton/src/FastCollinearPoints.java
@@ -63,18 +63,19 @@ public class FastCollinearPoints {
 				Arrays.sort(aux, c);
 
 				for (int j = 0; j < aux.length - 1; j++) {
-					double slope1 = points[i].slopeTo(aux[j]);
-					double slope2 = points[i].slopeTo(aux[j + 1]);
+					if (!isCollinear[j]) {
+						double slope1 = points[i].slopeTo(aux[j]);
+						double slope2 = points[i].slopeTo(aux[j + 1]);
 
-					if (slope1 == slope2) {
-						// found 3 collinear points, min point point index i in
-						// original array
-						Point min = points[i];
-						Point max = null;
+						if (slope1 == slope2) {
+							// found 3 collinear points, min point point index i
+							// in
+							// original array
+							Point min = points[i];
+							Point max = null;
 
-						for (int k = j + 2; k < aux.length; k++) {
-							// found the next collinear point
-							if (isCollinear[k] == false) {
+							for (int k = j + 2; k < aux.length; k++) {
+								// found the next collinear point{
 								Double slope3 = points[i].slopeTo(aux[k]);
 
 								if (slope1 == slope3) {
@@ -82,33 +83,36 @@ public class FastCollinearPoints {
 									// largest point of the collinear segment
 									max = aux[k];
 									isCollinear[k] = true;
-								}else
+								} else
 									break;
-								
-							} 
-						}
-
-						// if max == null then the fourth point is not found
-						if (max != null) {
-							isCollinear[j] = true;
-							isCollinear[j + 1] = true;
-							// check if this line segment has been added to the
-							// bag before
-							boolean alreadyAdded = false;
-
-							for (PointPair pp : bagPointPairs) {
-								/* Due to natural order of points, all subsequent subsegments
-								 * will have the same maximum point with the correct segment, and
-								 * the correct segment is always found first.
-								 */ 
-								if (max == pp.max)
-									if (slope1==pp.slope) {
-										alreadyAdded = true;
-										break;
-									}
 							}
-							if (!alreadyAdded)
-								bagPointPairs.add(new PointPair(min, max, slope1));
+
+							// if max == null then the fourth point is not found
+							if (max != null) {
+								isCollinear[j] = true;
+								isCollinear[j + 1] = true;
+								// check if this line segment has been added to
+								// the
+								// bag before
+								boolean alreadyAdded = false;
+
+								for (PointPair pp : bagPointPairs) {
+									/*
+									 * Due to natural order of points, all
+									 * subsequent subsegments will have the same
+									 * maximum point with the correct segment,
+									 * and the correct segment is always found
+									 * first.
+									 */
+									if (max == pp.max)
+										if (slope1 == pp.slope) {
+											alreadyAdded = true;
+											break;
+										}
+								}
+								if (!alreadyAdded)
+									bagPointPairs.add(new PointPair(min, max, slope1));
+							}
 						}
 					}
 				}
@@ -131,7 +135,7 @@ public class FastCollinearPoints {
 	private class PointPair {
 		Point min;
 		Point max;
-		double slope; //avoid recalculating slope
+		double slope; // avoid recalculating slope
 
 		public PointPair(Point x, Point y, double slope) {
 			this.min = x;
@@ -149,29 +153,33 @@ public class FastCollinearPoints {
 	 * @param p4
 	 * @return true if 4 points are collinear, false otherwise
 	 */
-//	private static boolean areCollinear(Point p1, Point p2, Point p3, Point p4) {
-//		double slope1 = p1.slopeTo(p2);
-//		double slope2 = p1.slopeTo(p3);
-//		double slope3 = p1.slopeTo(p4);
-//
-//		// if there are 3 equal points, then they must be linear
-//		if (slope1 == Double.NEGATIVE_INFINITY && slope2 == Double.NEGATIVE_INFINITY)
-//			return true;
-//		if (slope1 == Double.NEGATIVE_INFINITY && slope3 == Double.NEGATIVE_INFINITY)
-//			return true;
-//		if (slope2 == Double.NEGATIVE_INFINITY && slope3 == Double.NEGATIVE_INFINITY)
-//			return true;
-//		// if there are 2 equal points, one slope will be negative infinity
-//		if (slope1 == Double.NEGATIVE_INFINITY && slope2 == slope3)
-//			return true;
-//		if (slope2 == Double.NEGATIVE_INFINITY && slope1 == slope3)
-//			return true;
-//		if (slope3 == Double.NEGATIVE_INFINITY && slope1 == slope2)
-//			return true;
-//		// if the three slopes are equal, the 4 points are collinear.
-//		if (slope1 == slope2 && slope1 == slope3)
-//			return true;
-//
-//		return false;
-//	}
+	// private static boolean areCollinear(Point p1, Point p2, Point p3, Point
+	// p4) {
+	// double slope1 = p1.slopeTo(p2);
+	// double slope2 = p1.slopeTo(p3);
+	// double slope3 = p1.slopeTo(p4);
+	//
+	// // if there are 3 equal points, then they must be linear
+	// if (slope1 == Double.NEGATIVE_INFINITY && slope2 ==
+	// Double.NEGATIVE_INFINITY)
+	// return true;
+	// if (slope1 == Double.NEGATIVE_INFINITY && slope3 ==
+	// Double.NEGATIVE_INFINITY)
+	// return true;
+	// if (slope2 == Double.NEGATIVE_INFINITY && slope3 ==
+	// Double.NEGATIVE_INFINITY)
+	// return true;
+	// // if there are 2 equal points, one slope will be negative infinity
+	// if (slope1 == Double.NEGATIVE_INFINITY && slope2 == slope3)
+	// return true;
+	// if (slope2 == Double.NEGATIVE_INFINITY && slope1 == slope3)
+	// return true;
+	// if (slope3 == Double.NEGATIVE_INFINITY && slope1 == slope2)
+	// return true;
+	// // if the three slopes are equal, the 4 points are collinear.
+	// if (slope1 == slope2 && slope1 == slope3)
+	// return true;
+	//
+	// return false;
+	// }
 }
