28b968b8de389d665117fc3d17bc31df0d179d4f
nguyentrantrung986 <nguyentrantrung986@gmail.com>
2018-01-04 00:37:32 +0700

    Three way partition

---
+++ Algorithm_Princeton/src/exercises/ThreeWayPartition.java
@@ -0,0 +1,43 @@
+package exercises;
+
+import edu.princeton.cs.algs4.StdOut;
+import edu.princeton.cs.algs4.Stopwatch;
+
+public class ThreeWayPartition extends ElementarySort {
+	@SuppressWarnings({ "rawtypes" })
+	public static void sort(Comparable[] a, int lo, int hi) {
+		if(lo>=hi)
+			return;
+		
+		int i = lo;
+		int lt = lo;
+		int gt = hi;
+		Comparable v = a[lo];
+
+		while (true) {
+			if (less(a[i], v))
+				exchange(a, i++, lt++);
+			else if (less(v, a[i]))
+				exchange(a, i, gt--);
+			else if (v.equals(a[i]))
+				i++;
+			if (i >= gt)
+				break;			
+		}
+
+		sort(a, lo, lt - 1);
+		sort(a, lt + 1, hi);
+	}
+
+	// unit testing
+	public static void main(String[] args) {
+		Integer[] a = { 2148, 9058, 7742, 3153, 6324, 609, 7628, 5469, 7017, 504 };
+//		Integer[] a = getIntegerTestData("test\\integerRandomOrder.txt");
+		Stopwatch timer1 = new Stopwatch();
+		ThreeWayPartition.sort(a,0,a.length-1);
+		StdOut.println("Three way partitioning runs for " + timer1.elapsedTime() + " seconds");
+
+		for (int i : a)
+			System.out.print(i + " - ");
+	}
+}
