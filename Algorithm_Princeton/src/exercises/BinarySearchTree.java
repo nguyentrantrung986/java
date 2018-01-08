180e915b8c7577411b6c7153fa44f215e5f8c246
nguyentrantrung986 <nguyentrantrung986@gmail.com>
2018-01-08 18:51:11 +0700

    breath first search in binary search tree

--- Algorithm_Princeton/src/exercises/BinarySearchTree.java
+++ Algorithm_Princeton/src/exercises/BinarySearchTree.java
@@ -1,5 +1,7 @@
 package exercises;
 
+import java.util.ArrayList;
+
 import edu.princeton.cs.algs4.Queue;
 
 public class BinarySearchTree<Key extends Comparable<Key>, Value> {
@@ -47,18 +49,16 @@ public class BinarySearchTree<Key extends Comparable<Key>, Value> {
 
 	private String toString(Node nodeX) {
 		StringBuilder builder = new StringBuilder();
-		if (nodeX == null)
-			return "";
-
-		builder.append(nodeX.toString() + " ");
-		builder.append("left: " + nodeX.left + "	");
-		builder.append("right: " + nodeX.right);
-		builder.append(System.getProperty("line.separator"));
-
-		if (nodeX.left != null)
-			builder.append(toString(nodeX.left));
-		if (nodeX.right != null)
-			builder.append(toString(nodeX.right));
+		Iterable<Node> nodes = breathFirstIterator(nodeX);
+		int depth = 0;
+		
+		for(Node n: nodes){
+			if(depth < depth(n.key)){
+				System.out.println();
+				depth = depth(n.key);
+			}
+			System.out.print(n);
+		}
 
 		return builder.toString();
 	}
@@ -82,6 +82,62 @@ public class BinarySearchTree<Key extends Comparable<Key>, Value> {
 		inOrder(nodeX.right, q);
 	}
 
+	public Iterable<Key> breathFirstKeys() {
+		Iterable<Node> aln = breathFirstIterator(root);
+		Queue<Key> keys = new Queue<>();
+		for (Node n : aln) {
+			keys.enqueue(n.key);
+		}
+
+		return keys;
+	}
+	
+	/*The depth of a node is the number of edges from the root to the node.*/
+	public int depth(Key k){
+		return depth(root,k,0);
+	}
+	
+	private int depth(Node root, Key k, int depth){
+		if(k == null) return -1;
+		
+		int comp = k.compareTo(root.key);
+		if(comp < 0) return depth(root.left, k, depth+1);
+		else if(comp > 0) return depth(root.right, k, depth+1);
+		else return depth;
+	}
+
+	/**
+	 * Traverse the tree level by level, left to right
+	 * @param x
+	 * @return: list of nodes in breath first left to right order
+	 */
+	private Iterable<Node> breathFirstIterator(Node x) {
+		ArrayList<Node> al = new ArrayList<>();
+		al.add(x);
+		int i = 0; //the node in the current level whose children will be added next 
+		int j = 0; //the index of the latest node having been added
+		
+		int treeSize = size(x);
+
+		while (i < treeSize-1) {
+			if (x.left != null) {
+				al.add(x.left);
+				j++;
+			}
+			if (x.right != null) {
+				al.add(x.right);
+				j++;
+			}
+
+			if (i < j) {
+				i++;
+				x = al.get(i);
+			}
+		}
+
+		return al;
+	}
+
 	/**
 	 * Write a non-recursive traversal of a Binary Search Tree using constant
 	 * space and O(n) run time. This algorithm destroys the tree.
@@ -102,11 +158,50 @@ public class BinarySearchTree<Key extends Comparable<Key>, Value> {
 				parent = current;
 				current = current.left;
 			} else {
-				System.out.print(current+" ");
+				System.out.print(current + " ");
 				current = current.right;
 				parent = null;
 			}
 		}
+	}
+
+	public int height() {
+		return height(root);
+	}
+
+	/**
+	 * The height of a tree is the length of the path from the root to the
+	 * deepest node in the tree. A (rooted) tree with only a node (the root) has
+	 * a height of zero.
+	 * 
+	 * @param x
+	 * @return
+	 */
+	private int height(Node x) {
+		if (x == null)
+			return -1;
+
+		int lh = height(x.left);
+		int rh = height(x.right);
+
+		if (lh > rh)
+			return lh + 1;
+		else
+			return rh + 1;
+	}
+
+	public int size() {
+		return size(root);
+	}
+
+	private int size(Node x) {
+		if (x == null)
+			return 0;
+
+		int sizeL = size(x.left);
+		int sizeR = size(x.right);
+
+		return sizeL + sizeR + 1;
 	}
 
 	private class Node {
@@ -133,19 +228,24 @@ public class BinarySearchTree<Key extends Comparable<Key>, Value> {
 		bst.put("R", "R");
 		bst.put("C", "C");
 		bst.put("H", "H");
-		bst.put("G", "G");
 		bst.put("M", "M");
 
 		System.out.println(bst.toString());
+		System.out.println("Tree height: " + bst.height());
+		System.out.println("Tree size: " + bst.size());
 
-//		Iterable<String> it = bst.keys();
-//		for (String s : it)
-//			System.out.print(s + " ");
-		
-		bst.traverse();
+		Iterable<String> keys = bst.keys();
+		for (String s : keys)
+			System.out.print(s + " ");
 
 		System.out.println();
-		System.out.println("After traversing:");
-		System.out.println(bst.toString());
+
+		Iterable<String> bfKeys = bst.breathFirstKeys();
+		for (String s : bfKeys){
+			System.out.print(s + " ");
+		}
+		
+		System.out.println();
+		System.out.println("Depth H: "+ bst.depth("H"));
 	}
 }
