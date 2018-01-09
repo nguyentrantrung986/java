6238269f85e29950cae9430e626b5817994d0be7
nguyentrantrung986 <nguyentrantrung986@gmail.com>
2018-01-10 00:12:34 +0700

    kdtree calculating rect, draw

--- Algorithm_Princeton/src/KdTree.java
+++ Algorithm_Princeton/src/KdTree.java
@@ -1,16 +1,14 @@
 import edu.princeton.cs.algs4.Point2D;
 import edu.princeton.cs.algs4.RectHV;
-import edu.princeton.cs.algs4.SET;
+import edu.princeton.cs.algs4.StdDraw;
 
 public class KdTree {
 	private static final boolean DX = true;
 	private static final boolean DY = false;
 
-	private SET<Node> nodeSet;
 	private Node root;
 
 	public KdTree() {
-		nodeSet = new SET<Node>();
 	}
 
 	public boolean isEmpty() {
@@ -18,19 +16,29 @@ public class KdTree {
 	}
 
 	public int size() {
-		return nodeSet.size();
+		return size(root);
+	}
+	
+	private int size(Node x){
+		if(x == null) return 0;
+		
+		return size(x.lb) + size(x.rt) + 1;
 	}
 
 	public void insert(Point2D p) {
 		if (p == null)
 			throw new IllegalArgumentException();
 
-		root = insert(root, new Node(p), 0);
+		if(root == null){
+			root = new Node(p);
+			root.rect = new RectHV(0, 0, 1, 1);
+		}else
+			insert(root, new Node(p), 0);
 	}
 
 	// depth is the number of edges from root to the node, root depth = 0
-	private Node insert(Node root, Node newNode, int depth) {
-		if (root == null) {// not found newNode
+	private Node insert(Node parent, Node newNode, int depth) {
+		if (parent == null) {// not found newNode
 			if (depth % 2 == 0)
 				newNode.dividedBy = DX;
 			else
@@ -39,20 +47,48 @@ public class KdTree {
 			return newNode;
 		}
 
-		int compare = root.compareTo(newNode);
-		if (compare < 0){
-			root.lb = insert(root.lb, newNode, depth + 1);
-			
-			if(root.lb.rect == null){
-//				if(root.dividedBy==DX)
-//				root.lb.rect = new RectHV(root.rect.xmin(), root.rect.ymin(), , ymax)
+		int compare = parent.compareTo(newNode);
+		if (compare < 0) {
+			parent.lb = insert(parent.lb, newNode, depth + 1);
+
+			if (parent.lb.rect == null) {
+				double xmax = (parent.dividedBy == DX) ? parent.p.x() : parent.rect.xmax();
+				double ymax = (parent.dividedBy == DX) ? parent.rect.ymax() : parent.p.y();
+				parent.lb.rect = new RectHV(parent.rect.xmin(), parent.rect.ymin(), xmax, ymax);
+			}
+		} else if (compare > 0) {
+			parent.rt = insert(parent.rt, newNode, depth + 1);
+
+			if (parent.rt.rect == null) {
+				parent.rt.rect = new RectHV(parent.p.x(), parent.rect.ymin(), parent.rect.xmax(), parent.rect.ymax());
 			}
 		}
-		else if (compare > 0)
-			root.rt = insert(root.rt, newNode, depth + 1);
 
 		// if compare =0, the new node is found, do nothing
-		return root;
+		return parent;
+	}
+	
+	public void draw() {
+		draw(root);
+	}
+	
+	private void draw(Node x){
+		if(x == null) return;
+		StdDraw.setPenColor(StdDraw.BLACK);
+		StdDraw.setPenRadius(0.01);
+		x.p.draw();
+		
+		if(x.dividedBy == DX){
+			StdDraw.setPenColor(StdDraw.RED);
+			StdDraw.setPenRadius(0.003);
+			StdDraw.line(x.p.x(), x.rect.ymin(), x.p.x(), x.rect.ymax());
+		}else{
+			StdDraw.setPenColor(StdDraw.BLUE);
+			StdDraw.setPenRadius(0.003);
+			StdDraw.line(x.rect.xmin(),x.p.y(), x.rect.ymin(), x.p.y());
+		}
+		draw(x.lb);
+		draw(x.rt);
 	}
 
 	public boolean contains(Point2D p) {
@@ -104,10 +140,13 @@ public class KdTree {
 
 	public static void main(String[] args) {
 		KdTree kdt = new KdTree();
-		kdt.insert(new Point2D(0.1, 0.3));
+		kdt.insert(new Point2D(0.4, 0.3));
 		kdt.insert(new Point2D(0.3, 0.7));
-		kdt.insert(new Point2D(0.01, 0.7));
+		kdt.insert(new Point2D(0.8, 0.6));
 
 		System.out.println(kdt.contains(new Point2D(0.01, 0.7)));
+		System.out.println(kdt.size());
+		kdt.draw();
+		StdDraw.show();
 	}
 }
